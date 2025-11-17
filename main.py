2159dd349f34cade444123ff3cacdbc49b9f4c3f
NeriaJones <njone182@students.kennesaw.edu>
2025-11-17 01:44:02 -0500

    I added a negative budget function
    I added a code that makes users input valid numbers instead of negatives

--- main.py
+++ main.py
@@ -2,8 +2,14 @@ def set_monthly_budget():
     while True:
         try:
             budget = float(input("Enter your monthly budget amount: $"))
+
+            if budget < 0:
+                print("Budget cannot be negative. Please enter a valid number.")
+                continue
+
             print(f"Your monthly budget is set to: ${budget:.2f}")
             return budget
+
         except ValueError:
             print("Invalid input. Please enter a number.")
 
@@ -11,5 +17,6 @@ def main():
     print("Welcome to StyleSaver")
     budget = set_monthly_budget()
     print(f"Budget stored in main: ${budget:.2f}")
+
 if __name__ == "__main__":
     main()
